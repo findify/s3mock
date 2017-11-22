0709e70c1429fdce6da0d68d48a813a4d9283d53
Vahid Zoghi <vahid.zoghi@tradeshift.com>
2017-11-22 11:18:31 +0100

    Fix encoding problem which leads to mulitpart upload in alpakka

--- src/main/scala/io/findify/s3mock/response/CompleteMultipartUploadResult.scala
+++ src/main/scala/io/findify/s3mock/response/CompleteMultipartUploadResult.scala
@@ -1,5 +1,7 @@
 package io.findify.s3mock.response
 
+import java.net.URLDecoder
+
 /**
   * Created by shutty on 8/10/16.
   */
@@ -8,7 +10,7 @@ case class CompleteMultipartUploadResult(bucket:String, key:String, etag:String)
     <CompleteMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
       <Location>http://s3.amazonaws.com/{bucket}/{key}</Location>
       <Bucket>{bucket}</Bucket>
-      <Key>{key}</Key>
+      <Key>{/* the key is the still URLencoded path */URLDecoder.decode(key, "UTF-8") }</Key>
       <ETag>"{etag}"</ETag>
     </CompleteMultipartUploadResult>
 }
